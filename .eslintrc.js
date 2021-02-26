fd09340fe935d15c488249c08d400938a4fe6610
Uice Lu <uicestone@gmail.com>
2021-02-26 12:38:03 +0800

    add typescript support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,25 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
+
   extends: ['plugin:vue/essential'],
+
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'vue/no-use-v-if-with-v-for': "off",
     'vue/no-unused-vars': "off"
   },
+
   parserOptions: {
-    parser: 'babel-eslint'
-  }
+    parser: '@typescript-eslint/parser'
+  },
+
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/typescript'
+  ]
 };
