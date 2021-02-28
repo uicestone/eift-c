8f23795b431d5d98dba7101f799b6d2378f99acc
Uice Lu <uicestone@gmail.com>
2021-02-28 23:43:26 +0800

    dev port

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,25 @@
-const path = require('path');
+const path = require("path");
 
 function resolveSrc(_path) {
   return path.join(__dirname, _path);
 }
 // vue.config.js
 module.exports = {
+  devServer: {
+    host: "localhost",
+    port: "8084",
+  },
   lintOnSave: true,
   configureWebpack: {
     // Set up all the aliases we use in our app.
     resolve: {
       alias: {
-        assets: resolveSrc('src/assets')
-      }
-    }
+        assets: resolveSrc("src/assets"),
+      },
+    },
   },
   css: {
     // Enable CSS source maps.
-    sourceMap: process.env.NODE_ENV !== 'production'
-  }
+    sourceMap: process.env.NODE_ENV !== "production",
+  },
 };
