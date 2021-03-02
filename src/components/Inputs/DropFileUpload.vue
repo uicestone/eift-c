8a8afa6a8929e815a214a257b8d1f7c4f2162352
Uice Lu <uicestone@gmail.com>
2021-03-02 16:20:07 +0800

    drop file upload without dropzone

---
+++ src/components/Inputs/DropFileUpload.vue
@@ -0,0 +1,74 @@
+<template lang="pug">
+  .dropzone.mb-3(@drop.prevent="addDropFile", @dragover.prevent)
+    .dz-default.dz-message
+      button.dz-button(type='button') 把文件拖到这里上传
+    .preview.preview-single(v-if='!multiple')
+      .preview-cover
+        img.preview-img
+    ul.preview.preview-multiple.list-group.list-group-lg.list-group-flush(v-else)
+      li.list-group-item.px-0(v-for="file in files" :key="file.name")
+        .row.align-items-center
+          .col-auto
+            .avatar
+              img.avatar-img.rounded
+          .col.ml--3
+            h4.mb-1 {{file.name}}
+            p.small.text-muted.mb-0 {{file.createdAt|date}}
+          .col-auto
+            button.btn.btn-danger.btn-sm(@click="removeFile(file)")
+              i.fas.fa-trash
+</template>
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+import { File as FileDoc } from "@/resources/interfaces";
+import { http } from "@/resources";
+
+@Component({
+  model: {
+    prop: "value",
+    event: "change",
+  },
+})
+export default class DropFileUpload extends Vue {
+  @Prop({ type: Array })
+  value!: (File | FileDoc)[];
+
+  @Prop({ type: Boolean })
+  multiple!: boolean;
+
+  get files() {
+    return this.value;
+  }
+
+  set files(files) {
+    this.$emit("change", files);
+  }
+
+  addDropFile(e: DragEvent) {
+    if (!e.dataTransfer) return;
+    Array.from(e.dataTransfer.files).forEach(async (file) => {
+      const fileDoc = await this.uploadFile(file);
+      this.files.push(fileDoc);
+    });
+  }
+
+  async uploadFile(file: Blob) {
+    const formData = new FormData();
+    formData.append("file", file);
+    const fileDoc: FileDoc = (
+      await http.post("file", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+    ).data;
+    return fileDoc;
+  }
+
+  removeFile(file: File | FileDoc) {
+    this.files = this.files.filter((f) => f.name !== file.name);
+  }
+}
+</script>
+<style></style>
